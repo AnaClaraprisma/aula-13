e52c9fe0f3ce8cccd8d3cb4ce008bc8f15d25f0f
AnaClaraprisma <an4claracouto@gmail.com>
2024-08-22 15:05:48 -0300

    Update app.js

--- example/app.js
+++ example/app.js
@@ -4,19 +4,32 @@ function ContaBancaria(titular, saldo) {
   this.saldo = saldo;
 
   this.depositar = function (valor) {
-    if (valor > 0) {
+    if (valor >= 0) {
       this.saldo += valor
+    }
+    else{
+      console.log ('nao é possivel adicionaar um valor negativo')
     }
   },
 
-    this.retirar = function (valor) { },
+    this.retirar = function (valor) {
+      if (valor > 0) {
+        if (saldo > valor){
+          this.saldo -= valor
+        } else{
+          console.log('saldo insuficiente') 
+        }
+      }
+    },
 
     this.consultarSaldo = function () {
-      console.log(`O saldo de ${this.titular} é RS${this.saldo.toFixed(2)}`)
+      console.log(`O saldo de ${this.titular} é RS ${this.saldo.toFixed(2)}`)
     }
 };
 
 const AnaClara = new ContaBancaria('Ana Clara', 2500)
 
-AnaClara.depositar(700)
+AnaClara.depositar(-700)
 AnaClara.consultarSaldo()
+//AnaClara.retirar (6000)
+//AnaClara.consultarSaldo()
